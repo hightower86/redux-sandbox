2238a25a9cd5e6158e35fe79f94bec24448ba100
Adel <htwr86@gmail.com>
2019-07-11 20:14:26 +0300

    106 reducer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 
-console.log('hello world');
 
+const reducer = (state = 0, action) => {
+
+  switch(action.type) {
+    case 'INC':
+      return state + 1;
+    default:
+      return state;
+  }
+   
+};
+
+let state = reducer(undefined, { })
