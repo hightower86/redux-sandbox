3de59e638432eab697d9c4968237875235914971
Adel <htwr86@gmail.com>
2019-07-12 14:04:28 +0300

    107.99 redux store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-
+import { createStore } from 'redux';
 
 const reducer = (state = 0, action) => {
 
   switch(action.type) {
     case 'INC':
       return state + 1;
+    case 'INC10':
+      return state + 10;
     default:
       return state;
   }
    
 };
 
-let state = reducer(undefined, { })
+const store = createStore(reducer);
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+store.dispatch({type:'INC'});
+store.dispatch({type:'INC'});
+store.dispatch({type:'INC10'});
+store.dispatch({type:'INC'});
+store.dispatch({type:'INC'});
