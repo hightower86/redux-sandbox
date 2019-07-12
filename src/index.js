82d6d4bb081086cbe12221271e0d6791690124bc
Adel <htwr86@gmail.com>
2019-07-12 18:39:50 +0300

    109.7 UI for redux

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const reducer = (state = 0, action) => {
   switch(action.type) {
     case 'INC':
       return state + 1;
-    case 'INC10':
-      return state + 10;
+    case 'DEC':
+      return state - 1;
     default:
       return state;
   }
@@ -16,10 +16,15 @@ const reducer = (state = 0, action) => {
 const store = createStore(reducer);
 store.subscribe(() => {
   console.log(store.getState());
-})
+});
 
-store.dispatch({type:'INC'});
-store.dispatch({type:'INC'});
-store.dispatch({type:'INC10'});
-store.dispatch({type:'INC'});
-store.dispatch({type:'INC'});
+document
+  .getElementById('inc')
+  .addEventListener('click', () => {
+    store.dispatch({type: 'INC'});
+  });
+document
+  .getElementById('dec')
+  .addEventListener('click', () => {
+    store.dispatch({type: 'DEC'});
+  });
